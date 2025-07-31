ec914a93631dee016959e7c4b205641cd79a6644
nivedita <nivedita.rani19@gmail.com>
2025-07-31 16:22:18 +0200

    fixed issues related to favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,9 @@ export const metadata = {
   ],
   icons: {
     icon: [
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", type: "image/x-icon" },
       { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
       { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
-      { url: "/favicon.ico", type: "image/x-icon" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }, 
