2ffddd485ef466622494b9c3a2245f7409745cec
nivedita <nivedita.rani19@gmail.com>
2025-11-07 11:25:03 +0100

    fixing nested footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="text-center text-sm text-gray-500 m-2">
+        <div className="text-center text-sm text-gray-500 m-2">
             <p>© 2025 Nivedita Rani. All rights reserved.</p>
-        </footer>
+        </div>
     );
 }
